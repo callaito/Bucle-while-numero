ed981adc9ca60b4ec1dda1e053d00bb11461e95e
callaito <radelinrsm029@gmail.com>
2024-03-07 20:27:46 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Imprimir en pantalla del 1 al 10");
+Console.WriteLine();
+
+int i = 1;
+
+while (i <=10)
+{
+    Console.WriteLine("Numero "+i);
+    i = i+1;
+}
+
+
+
+
